9d3cb9f21c7bc1ef42af538f769924432081266c
hiagopsilva <hiagohps360@gmail.com>
2020-08-13 23:26:49 -0300

    add components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,44 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
 
 import GlobalStyles from './styles/GlobalStyles';
-import Routes from './routes/index';
+import Section from './components/Section';
+import SideMenu from './components/SideMenu';
+import MenuForm from './components/MenuForm';
+
+import data from './data';
 
 const App: React.FC = () => (
   <>
-    <BrowserRouter>
-      <Routes />
-    </BrowserRouter>
+    <Section
+      variant="blue"
+      title={data[0].title}
+      description={data[0].description}
+    />
+    <Section
+      variant="beige"
+      title={data[1].title}
+      description={data[1].description}
+    />
+    <Section
+      variant="blue"
+      title={data[2].title}
+      description={data[2].description}
+    />
+    <Section
+      variant="white"
+      title={data[3].title}
+      description={data[3].description}
+    />
+    <Section
+      variant="black"
+      title={data[4].title}
+      description={data[4].description}
+    />
+
+    <SideMenu>
+      <MenuForm />
+    </SideMenu>
+
     <GlobalStyles />
   </>
 );
